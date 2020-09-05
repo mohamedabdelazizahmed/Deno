60ce8d6f3a008ad03ebf6340851bf7753fbe47cc
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-09-05 22:41:14 +0200

    43-add-headers-body-redirect-more-info-request

--- dummy_response_server.ts
+++ dummy_response_server.ts
@@ -1,17 +1,25 @@
-//import { server } from "./my_server.ts"; 
-import defaultServer from './my_server.ts';
+//import { server } from "./my_server.ts";
+import defaultServer from "./my_server.ts";
 console.log("http://localhost:3000/");
-for await (const req of defaultServer) {
-  const headers = new Headers();
-  // to make clear in browser sending back some html code
-  headers.set('Content-Type' ,'text/html'); 
-  const body  =  `<h2>First App by Deno :) </h2>
-  <form>
-  <input type="text"/>
-  <button>submit </button>
-  </form>`
-  req.respond({ body: body, headers:headers });
-}
+for await (const request of defaultServer) {
+  console.log(request.method);
+  console.log(request.url);
 
+  if (request.method == "POST" && request.url == "/store-message") {
+    const headers = new Headers();
+    headers.set("Location", "/confirm");
+    request.respond({ headers: headers, status: 303 }); //redirect status code
+  } else {
+    const headers = new Headers();
+    // to make clear in browser sending back some html code
+    headers.set("Content-Type", "text/html");
+    const body = `<h2>First App by Deno :) </h2>
+    <form action="/store-message" method="POST">
+    <input type="text" name="message"/>
+    <button type="submit">submit </button>
+    </form>`;
+    request.respond({ body: body, headers: headers });
+  }
+}
 
 //> deno run --allow-net dummy_response_server.ts
