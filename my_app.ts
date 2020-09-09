acd312d8d1ec38a7bce3d76c53d603cbcb6b77c8
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-09-09 22:10:32 +0200

    48-use-restrict-type-in-typescript

--- my_app.ts
+++ my_app.ts
@@ -1,12 +1,22 @@
-function parseBuffer(buffer:any) {
-    const decoder = new TextDecoder();
-    const decodedContent = decoder.decode(buffer);
-    return decodedContent;
+/**
+ *  use TypeScript you use restrict type 
+ *  buffer not use any [unspecific]
+ * use Uint8Array instead of any type
+ */
+function parseBuffer(buffer: Uint8Array) {
+  const decoder = new TextDecoder();
+  const decodedContent = decoder.decode(buffer);
+  return decodedContent;
 }
 const inputBuffer = new Uint8Array();
 await Deno.stdin.read(inputBuffer);
-const content = parseBuffer(inputBuffer.toString());
+
+// remove toString because you use buffer of type Uint8Array
+const content = parseBuffer(inputBuffer);
+console.log(content);
 
 // different type pass in writeFile
-await Deno.writeFile('user-input.txt' , inputBuffer , {create: false});
+await Deno.writeFile("user-input.txt", inputBuffer, { create: false });
 console.log("Done");
+
+// in official deno manuel go to style guide  section
