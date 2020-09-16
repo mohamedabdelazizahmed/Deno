18bc032fd4d5c79f327df7af433c1238fc3b0b46
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-09-16 21:33:56 +0200

    58-Understanding Multiple Middleware Functions

--- app.ts
+++ app.ts
@@ -3,12 +3,16 @@ import { Application } from "https://deno.land/x/oak@v6.2.0/mod.ts";
 
 const app = new Application();
 // [middleware]function executed for every request
-app.use((ctx, next) => {
+app.use(async(ctx, next) => {
   console.log("Middleware 1");
   console.log(ctx.request.method);
   console.log(ctx.request.url);
   ctx.response.body = "Message From Middleware ";
-  
+    await next(); // to go next middleware 2 after middleware 2 do console After Next
+    console.log('After next');
 });
-
+app.use((ctx, next) => {
+  console.log("Middleware 2");
+  ctx.response.body = "Message From Middleware 2";
+});
 app.listen({ port: 3000 });
