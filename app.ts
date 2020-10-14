6928ee30a2421f8642e4c68c03ce2173c002bc24
mohamedabdelazizahmed <mr.mohamedabdelaziz@gmail.com>
2020-10-14 16:48:05 +0200

    Rendering Lists of Data

--- app.ts
+++ app.ts
@@ -5,24 +5,34 @@ import { renderFileToString ,renderFile } from 'https://deno.land/x/dejs@0.8.0/m
 const app = new Application();
 const router = new Router();
 
+const courseGoals:{name:string; id:string}[] = [];
 // add new Route for www.test.com
 router.get("/", async (ctx) => {
   const body = await renderFileToString(Deno.cwd() + "/course_goals.ejs", {
     title: "MyGoals",
+    goals:courseGoals
   });
   ctx.response.body = body;
 });
 
 router.post("/add-goal", async (ctx) => {
-  //get data from body
-  const body = await ctx.request.body();
-  ctx.response.redirect("/");
+  if (ctx.request.hasBody) {
+    const body = ctx.request.body();
+    console.log(body);
+      const value = await body.value; // an object of parsed JSON
+      const newGoalTitle = value.get('newGoal');
+      // console.log(value.get('newGoal'));
+      console.log(newGoalTitle);
+      const newGoal = {id: new Date().toISOString() , name: newGoalTitle};
+      console.log(newGoal);
+      courseGoals.push(newGoal);
+      ctx.response.redirect('/');  
+  }
+
 });
 
-app.use((ctx, next) => {
-  console.log("My Own middleware before router.routes");
-  next();
-});
+
+
 // to Register router and run it
 app.use(router.routes());
 app.use(router.allowedMethods());
@@ -34,17 +44,4 @@ app.use(async(ctx)=>{
   await send(ctx , ctx.request.url.pathname); //pathname =>/app.css
 })
 
-// [middleware]function executed for every request
-// app.use(async(ctx, next) => {
-//   console.log("Middleware 1");
-//   console.log(ctx.request.method);
-//   console.log(ctx.request.url);
-//   ctx.response.body = "Message From Middleware ";
-//     await next(); // to go next middleware 2 after middleware 2 do console After Next
-//     console.log('After next');
-// });
-// app.use((ctx, next) => {
-//   console.log("Middleware 2");
-//   ctx.response.body = "Message From Middleware 2";
-// });
 app.listen({ port: 3000 });
